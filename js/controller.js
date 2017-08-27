f853457cb84baa3db7b45f8daed473b3ccbfa199
Alexpham688 <Alexpham688@gmail.com>
2017-08-27 19:55:26 -0400

    another commit

--- js/controller.js
+++ js/controller.js
@@ -1,17 +1,31 @@
 app.controller("myController", function($scope) {
  $scope.ticketBox = [];
- $scope.item= {fname: "",lName: "",email:"", number:1, total: ""};
- $scope.addItem = function(fName, lName, email, number, total) {
-    console.log("submit", fName, lName, email, number, total );
-    var fedItem = {fname:fName, lName:lName, email:email, number:number, total:total};
+ $scope.today = new Date();
+ $scope.item= {fname: "",lName: "",email:"", number:1};
+ $scope.addItem = function(fName, lName, email, number) {
+    console.log("submit form", fName, lName, email, number);
+    var fedItem = {fName:fName, lName:lName, email:email, number:number};
 
-    if(number > 1) {
-      fedItem.extra = "s";
+    if (fName === "") {
+        alert("Enter First Name");
+
+    } else if (lName === "") {
+        alert("Enter Last Name");
+
+    } else if (email !== "") {
+        alert("Enter Email");
+        return true;
+
+    } else if(number > 1) {
+        fedItem.extra = "s";
+
     } else {
-      feditem.extra = "";
+        fedItem.extra = "";
     }
 
-    $scope.ticketbox.push(fedItem);
-    $scope.item = {fName: "",lName: "", email:"",number:1,total:""};
+
+
+    $scope.ticketBox.push(fedItem);
+    $scope.item = {fName: "",lName: "", email:"",number:1};
 };
 });
