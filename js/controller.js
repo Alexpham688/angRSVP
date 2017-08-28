024610d4a722f33a2b0d1f5838bf18c788df58c5
Alexpham688 <Alexpham688@gmail.com>
2017-08-28 15:09:31 -0400

    added storage

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,15 @@
-app.controller("myController", function($scope) {
+app.controller("myController", function($scope, $localStorage) {
  $scope.ticketBox = [];
+ //creates date function
  $scope.today = new Date();
  $scope.item= {fname: "",lName: "",email:"", number:1};
  $scope.addItem = function(fName, lName, email, number) {
-    console.log("submit form", fName, lName, email, number);
+     //create var fedItem to store info
     var fedItem = {fName:fName, lName:lName, email:email, number:number};
 
+
+
+  // alert for if blank input field
     if (fName === "") {
         alert("Enter First Name");
 
@@ -15,17 +19,18 @@ app.controller("myController", function($scope) {
     } else if (email === "") {
         alert("Enter Email");
         return true;
-
+    // if number is higher than 1 make plural
     } else if(number > 1) {
         fedItem.extra = "s";
 
     } else {
         fedItem.extra = "";
     }
-
-
-
+    //localStorage
+    $localStorage.fedItem = fedItem;
+      //pushes items to box
     $scope.ticketBox.push(fedItem);
+     //resets inputs
     $scope.item = {fName: "",lName: "", email:"",number:1};
 };
 });
